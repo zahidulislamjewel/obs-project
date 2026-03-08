75b5fb4e608c127ace8e65c9452f67857229df80
zahidulislamjewel <zahidulislam.jewel@gmail.com>
2026-03-08 19:32:06 +0600

    feat: add CategoryFormComponent

---
+++ obs-frontend/obs-frontend/src/app/components/category-form/category-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+
+@Component({
+  selector: 'app-category-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './category-form.component.html',
+  styleUrl: './category-form.component.css'
+})
+export class CategoryFormComponent {
+  formData = { name: '', description: '' };
+
+  constructor(private categoryService: CategoryService, private router: Router) {}
+
+  onSubmit(): void {
+    this.categoryService.createCategory(this.formData).subscribe(() => {
+      this.router.navigate(['/categories']);
+    });
+  }
+}
