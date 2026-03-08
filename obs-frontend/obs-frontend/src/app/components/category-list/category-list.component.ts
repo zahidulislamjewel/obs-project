2800166b5b08d0c465b13cab9d820c1fe75b1021
zahidulislamjewel <zahidulislam.jewel@gmail.com>
2026-03-08 19:32:35 +0600

    feat: add CategoryListComponent

---
+++ obs-frontend/obs-frontend/src/app/components/category-list/category-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category';
+
+@Component({
+  selector: 'app-category-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './category-list.component.html',
+  styleUrl: './category-list.component.css'
+})
+export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(c => this.categories = c);
+  }
+}
